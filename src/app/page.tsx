541938ba34d473abdb0f17deec448787fa3f1535
Shamiullah Khan <110717400+shamikhan005@users.noreply.github.com>
2024-08-10 15:55:33 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center text-center">
       <h1 className="font-bold tex-2xl my-4">about</h1>
-      <p className="mb-4">hello! i'm shami, currently a computer science undergrad.</p>
+      <p className="mb-4">hello! i&apos;m shami, currently a computer science undergrad.</p>
       <div className="space-x-4 underline">
         <Link href="https://github.com/shamikhan005">
           github
